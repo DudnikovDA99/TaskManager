d077952470e18fb2b8507abf104a7995174b24b9
Quu-Bee <QuuBeeM@Gmail.com>
2026-02-19 20:19:21 +0700

    #3 (#4)
    * [#3] feat: add priority to a task
    
    * hotfix: remove created_by from task editing form in admin panel
    
    * [#3] fix: fix imports

--- TaskManager/TaskManager/admin.py
+++ TaskManager/TaskManager/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
-
 from .models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "created_at", "updated_at")
+    list_display = ("name", "description", "created_at", "updated_at", )
 
     def save_model(self, request, obj, form, change):
         if not obj.created_by:
             obj.created_by = request.user
 
         super().save_model(request, obj, form, change)
+
+    exclude = ("created_by",)
 
 
 admin.site.register(Task, TaskAdmin)
