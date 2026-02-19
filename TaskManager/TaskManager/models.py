afc703281409f7a23b9f98cb558ccf97b5d58658
SANYA <QuuBeeM@Gmail.com>
2026-02-19 20:08:03 +0700

    [#3] feat: add priority to a task

--- TaskManager/TaskManager/models.py
+++ TaskManager/TaskManager/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.conf import settings
+from django.db import models
 from django.contrib.auth.models import User
 
 
 class Task(models.Model):
+    class Priority(models.IntegerChoices):
+        MAIN_QUEST = 1, "Main_quest"
+        SIDE_QUEST = 2, "Routine"
+        DAILY = 3, "Daily"
+        GRIND = 4, "Grind"
+
+    priority = models.IntegerField(
+        choices=Priority.choices,
+        default=Priority.SIDE_QUEST,
+    )
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, blank=True
     )
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
