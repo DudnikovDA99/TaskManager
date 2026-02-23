8b32c95a5c10fa0db96e938fd322fad7df123ab1
Quu-Bee <QuuBeeM@Gmail.com>
2026-02-23 19:32:04 +0700

    [#9] feat: add subtasks (#12)
    * [#9] feat: add subtask
    
    * [#9] style: black format

--- TaskManager/TaskManager/admin.py
+++ TaskManager/TaskManager/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Subtask
+from django.utils.html import format_html
+from django.urls import reverse
+
+
+class SubtaskInline(admin.TabularInline):
+    model = Subtask
+    extra = 1
+    can_delete = False
+    readonly_fields = [
+        "edit_link",
+    ]
+
+    def get_fields(self, request, obj=None):
+        fields = [
+            "edit_link",
+        ]
+        if obj is not None:
+            fields.append("is_completed")
+        return fields
+
+    def edit_link(self, obj):
+        """Возвращает ссылку на редактирование подзадачи с её названием."""
+        if obj.pk:  # для уже существующих объектов
+            # Формируем URL для изменения объекта Subtask
+            url = reverse(
+                f"admin:{obj._meta.app_label}_{obj._meta.model_name}_change",
+                args=[obj.pk],
+            )
+            return format_html('<a href="{}">{}</a>', url, obj.name)
+        return "—"  # для новых, ещё не сохранённых записей
+
+    edit_link.short_description = " "  # Заголовок колонки
 
 
 class TaskAdmin(admin.ModelAdmin):
@@ -12,8 +44,14 @@ class TaskAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
     exclude = ("created_by",)
+    inlines = [SubtaskInline]
+
+
+@admin.register(Subtask)
+class SubtaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_completed")
+    exclude = ("order", "task")
 
 
 admin.site.register(Task, TaskAdmin)
-
 # Register your models here.
