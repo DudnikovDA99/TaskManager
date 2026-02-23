d2cbc36e2d73f001707ce6794fbbd5b7c53a6ab5
SANYA <QuuBeeM@Gmail.com>
2026-02-23 19:18:18 +0700

    [#9] feat: add subtask

--- TaskManager/TaskManager/models.py
+++ TaskManager/TaskManager/models.py
@@ -17,7 +17,32 @@ class Task(models.Model):
     created_by = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, blank=True
     )
-    description = models.TextField()
+    description = models.TextField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Subtask(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(default="")
+    is_completed = models.BooleanField(default=False)
+    order = models.PositiveBigIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # новый объект
+            max_order = Subtask.objects.filter(task=self.task).aggregate(
+                models.Max("order")
+            )["order__max"]
+            self.order = (max_order or 0) + 1
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return f"subtask {self.order}"
