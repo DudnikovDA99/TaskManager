595cc6d6c52ba7b5dc404fa5bea89e94e9f6dfab
Dmitry <korkanovichm@yandex.ru>
2026-03-10 17:07:33 +0300

    [#11] feat: add task drf view

--- TaskManager/TaskManager/views.py
+++ TaskManager/TaskManager/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = []
